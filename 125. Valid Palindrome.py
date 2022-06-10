c3f70165cd92de2801b3a0d9437d380165af8c57
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-06-10 23:56:41 +0900

    [add]list

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,5 +1,15 @@
 import re
 s = "A man, a plan, a canal: Panama"
+# 리스트 사용 317 ms
+strs = []
+for i in s:
+    if i.isalnum():
+        strs.append(i.lower())
+while len(strs) > 1:
+    if strs.pop() != strs.pop(0):
+        print(False)
+
+print(True)
 
 
 
@@ -7,8 +17,8 @@ s = "A man, a plan, a canal: Panama"
 
 
 #re모듈 사용 52ms
-s = s.lower()
-s = re.sub('[^a-z0-9]','',s)
-
-if s == s[::-1]:
-    print(True)
+# s = s.lower()
+# s = re.sub('[^a-z0-9]','',s)
+#
+# if s == s[::-1]:
+#     print(True)
