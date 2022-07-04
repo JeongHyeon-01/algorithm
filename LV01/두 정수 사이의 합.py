a4e42b1aaeba6081291cb4a93dd7688df1b8a76b
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-07-04 15:46:50 +0900

    7/04

---
+++ LV01/두 정수 사이의 합.py
@@ -0,0 +1,9 @@
+'''
+두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+'''
+
+a = 5
+b = 3
+start, end = min(a,b), max(a,b)+1
+print(sum(range(start, end)))
